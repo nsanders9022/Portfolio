f40535a9de273b730cac223380a9a2088d8bf3c6
Nicole Sanders <nsanders9022@gmail.com>
2017-07-08 16:48:31 -0700

    add pictures for Sizzle Stuff project

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+  //picture hover
+  $('#homeHeader-thumbnail').hover(function() {
+    $('#homeHeader-big').toggle();
+  })
+  $('#homeProducts-thumbnail').hover(function() {
+    $('#homeProducts-big').toggle();
+  })
+  $('#productPage-thumbnail').hover(function() {
+    $('#productPage-big').toggle();
+  })
+  $('#reviewCart-thumbnail').hover(function() {
+    $('#reviewCart-big').toggle();
+  })
+
+
+})
